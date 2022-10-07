18e0b9a20ee1e677b80f6eb55f01f86829ade8ab
FawazdeenAbimbola <112256507+FawazdeenAbimbola@users.noreply.github.com>
2022-10-07 01:34:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById('navresponsive').style.width = "100%";
+}
+function closeNav() {
+    document.getElementById('navresponsive').style.width = "0%";
+}
